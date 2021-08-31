3dbc4bf58b91b6a8634ac528d30e2bfb33e5f64e
ljoecordoba <ljoecordoba@gmail.com>
2021-08-31 12:03:23 -0300

    First commit

---
+++ pythonya/basics-syntax/sequential-programming.py
@@ -0,0 +1,5 @@
+numero1 = int(input("Ingrese un numero:"))
+numero2 = int(input("Ingrese otro numero:"))
+
+print("La suma es : " + str(numero1 + numero2))
+print("El producto es : " + str(numero1 * numero2))
